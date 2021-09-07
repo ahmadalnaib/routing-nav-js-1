c0b463a07a0de4a16095dcdf0675854125b51acf
ahmedhabeeb73 <alnaib888@gmail.com>
2021-09-07 16:47:52 +0200

    check the file

--- contact.php
+++ contact.php
@@ -1,11 +1,34 @@
 <?php
 $title = "contact";
 require_once('./template/header.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      if (isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
+            $allowedFiles = [
+                  'jpg' => 'image/jpeg',
+                  'png' => 'image/png',
+                  'gif' => 'image/gif'
+            ];
+
+            $fileType = mime_content_type($_FILES['file']['tmp_name']);
+
+            if (!in_array($fileType, $allowedFiles)) {
+                  echo 'file type not support';
+
+            }
+      }
+}
+
+
+
+
+
+
 ?>
 <div class="content">
       <div class="container-form">
             <h1>Contact us</h1>
-            <form action="" method="post">
+            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
                   <div>
                         <input type="text" placeholder="Name">
                   </div>
@@ -13,11 +36,12 @@ require_once('./template/header.php');
                         <input type="email" placeholder="Email">
                   </div>
                   <div>
-                        <input type="file"  name="file">
+                        <input type="file" name="file">
                   </div>
                   <div>
-                      <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                        <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
                   </div>
+                  <button class="btn-primary">Send</button>
             </form>
       </div>
 </div>
--- template/footer.php
+++ template/footer.php
@@ -3,7 +3,7 @@
       <ul>
         <li><i class="fas fa-home"></i> <a href="index.php">Home</a></li>
         <li><i class="fas fa-user-alt"></i>About</li>
-        <li><i class="fas fa-envelope">  <a href="contact.php">Contact</a></i></li>
+        <li><i class="fas fa-envelope"> <a href="contact.php">Contact</a></i></li>
       </ul>
     </nav>
 
